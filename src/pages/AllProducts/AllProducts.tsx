5e87488e249c5105b3dcc3624ab92c43db03c036
MDMahidul <mmbmahidul007@gmail.com>
2024-07-13 17:39:28 +0600

    filter data by category and sort data by price

--- src/pages/AllProducts/AllProducts.tsx
+++ src/pages/AllProducts/AllProducts.tsx
@@ -7,7 +7,6 @@ import SectionHeader from "@/components/Headers/SectionsHeader";
 import Loader from "@/components/Loader/Loader";
 import { useGetAllProductsQuery } from "@/redux/api/baseApi";
 import { TProduct } from "@/types/types";
-import { X } from "lucide-react";
 
 type RouteParams = {
   category?: string;
@@ -17,16 +16,20 @@ const AllProducts= () => {
   const { category } = useParams<RouteParams>();
   const navigate = useNavigate();
   const [name, setName] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState(category);
   const [queryOptions, setQueryOptions] = useState<{
     category?: string;
     name?: string;
+    sort?: string;
   }>({});
 
   // reload the page based on the category data
   useEffect(() => {
     if (category) {
+      setSelectedCategory(category.toLowerCase());
       setQueryOptions({ category: category.toLowerCase() });
     } else {
+      setSelectedCategory(undefined)
       setQueryOptions({});
     }
   }, [category]);
@@ -46,7 +49,7 @@ const AllProducts= () => {
     }));
     // Update URL based on search params
     navigate(
-      `/all-products${category ? `/${category}` : ""}${
+      `/all-products${selectedCategory ? `/${selectedCategory}` : ""}${
         name ? `?name=${name}` : ""
       }`
     );
@@ -60,8 +63,16 @@ const AllProducts= () => {
         name: undefined,
       }));
       // Update URL to remove search params
-      navigate(`/all-products${category ? `/${category}` : ""}`);
+      navigate(`/all-products${selectedCategory ? `/${selectedCategory}` : ""}`);
     };
+
+    // handle sort by price
+   const handleSortByPrice = (sortType: string) => {
+     setQueryOptions((prevOptions) => ({
+       ...prevOptions,
+       sort: sortType,
+     }));
+   };
 
   const { data, error, isLoading } = useGetAllProductsQuery(queryOptions, {
     pollingInterval: 30000,
@@ -78,7 +89,7 @@ const AllProducts= () => {
           <div className="pt-12 md:pt-24">
             <p className="mt-20 py-40 text-center text-xl font-semibold text-primary">
               No products found for{" "}
-              {category ? `${category} category` : "the selected criteria"}.
+              {selectedCategory ? `${selectedCategory} category` : "the selected criteria"}.
             </p>
           </div>
         </Container>
@@ -98,40 +109,93 @@ const AllProducts= () => {
 
   const { data: products } = data;
 
+  // get the sorted data
+    const sortedProducts = [...products].sort((a: TProduct, b: TProduct) => {
+      if (queryOptions.sort === "price") {
+        return a.price - b.price;
+      } else if (queryOptions.sort === "-price") {
+        return b.price - a.price;
+      } else {
+        return 0;
+      }
+    });
+
   return (
-    <div className={`${category ? "pt-10 md:pt-16" : "pt-28 md:pt-36"}`}>
-      {category && <SectionHeader heading={`${category} Products`} />}
+    <div className="pt-10 md:pt-16">
+      <SectionHeader
+        heading={category ? `${category} Products` : "All Products"}
+      />
       <Container>
-        <div>
-          <form
-            onSubmit={handleSearchSubmit}
-            className="flex items-center gap-4 mb-5"
-          >
-            <div className="relative">
-              <input
-                className="bg-gray-50 border border-secondary text-gray-900 rounded-lg block p-2.5 focus:outline-none focus:border-blue-600 focus:ring-blue-600 w-60 "
-                type="text"
-                placeholder="Search by name"
-                value={name}
-                onChange={handleNameChange}
-              />
-              {name && (
-                <button
-                  type="button"
-                  onClick={handleClearSearch}
-                  className="secondary-button absolute right-5 top-3.5 text-tertiary"
-                >
-                  <X className="size-4" />
-                </button>
-              )}
+        <FadeInUpAnimation>
+          <div className="flex flex-col md:flex-row justify-center items-center  md:justify-between">
+            <form
+              onSubmit={handleSearchSubmit}
+              className="flex flex-col md:flex-row items-center gap-4 mb-5"
+            >
+              <div className="relative">
+                <input
+                  className="bg-gray-50 border border-secondary text-gray-900 rounded-lg block p-2.5 focus:outline-none focus:border-blue-600 focus:ring-blue-600 w-60 "
+                  type="text"
+                  placeholder="Search by name"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+                {name && (
+                  <button
+                    type="button"
+                    onClick={handleClearSearch}
+                    className="secondary-button absolute right-5 top-3.5 text-tertiary"
+                  >
+                    <X className="size-4" />
+                  </button>
+                )}
+              </div>
+              <button type="submit" className="primary-button">
+                Search
+              </button>
+            </form>
+            <div className="mb-5">
+              <select
+                className="bg-gray-50 border border-secondary text-gray-500 rounded-lg block p-2.5 focus:outline-none focus:border-blue-600 focus:ring-blue-600 w-60"
+                value={selectedCategory || ""}
+                onChange={(e) => {
+                  const selected = e.target.value;
+                  setSelectedCategory(selected);
+                  if (selected) {
+                    setQueryOptions({ category: selected });
+                    navigate(`/all-products/${selected}`);
+                  } else {
+                    setSelectedCategory(undefined);
+                    setQueryOptions({});
+                    navigate("/all-products");
+                  }
+                }}
+              >
+                <option value="">Select Category</option>
+                <option value="football">Football</option>
+                <option value="basketball">Basketball</option>
+                <option value="cricket">Cricket</option>
+                <option value="tennis">Tennis</option>
+                <option value="golf">Golf</option>
+                <option value="baseball">Baseball</option>
+              </select>
             </div>
-            <button type="submit" className="primary-button">
-              Search
-            </button>
-          </form>
-        </div>
+            <div className="flex flex-col-reverse md:flex-row items-center mb-5 gap-1">
+              <label className="mr-2 text-gray-700">Sort by Price:</label>
+              <select
+                className="bg-gray-50 border border-secondary text-gray-500 rounded-lg block p-2.5 focus:outline-none focus:border-blue-600 focus:ring-blue-600"
+                value={queryOptions.sort || ""}
+                onChange={(e) => handleSortByPrice(e.target.value)}
+              >
+                <option value="">Default</option>
+                <option value="price">Low to High</option>
+                <option value="-price">High to Low</option>
+              </select>
+            </div>
+          </div>
+        </FadeInUpAnimation>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-5 md:gap-y-10">
-          {products.map((product: TProduct, index: number) => (
+          {sortedProducts.map((product: TProduct, index: number) => (
             <FadeInUpAnimation custom={index} key={product._id}>
               <ProductCard product={product} />
             </FadeInUpAnimation>
